2b7601bc4891c5021f55442b140f9d55dd70ebe8
PetercSnk <pcsnook@btinternet.com>
2019-03-31 21:01:51 +0100

    values off

--- angles.py
+++ angles.py
@@ -24,6 +24,7 @@ def open_queries():
 def main():
     
     history_vec = history_vectors()
+    query_entries = open_queries()
     
     avg_angle = average_angle()
     positive_entries = 0
@@ -35,23 +36,96 @@ def main():
                 
     history_vec = convert_history_to_np_dtype()        
     query_vec = convert_queries_to_np_dtype()    
+    query_vec_ind = convert_queries_to_np_dtype_individual()
     
-    print(query_vec, history_vec)
+    #print(query_vec, history_vec)
     
-    for all_query_vec in query_vec:
+    all_angles = []
+    
+    for all_query_vec in query_vec_ind:
+        
+        angles_for_query = []
         
         for all_history_vec in history_vec:
             
             if np.any(np.not_equal(all_query_vec, all_history_vec)):
                 
                 print(all_query_vec, all_history_vec)
-                print(calculate_angle(all_query_vec, all_history_vec))
+                
+                angles_for_query.append(calculate_angle(all_query_vec, all_history_vec))
+                
+                #print(all_query_vec, all_history_vec)
+                #print(calculate_angle(all_query_vec, all_history_vec))
+     
+        all_angles.append(angles_for_query)
+               
+    #print(all_angles)
+   
+    print(all_angles)
+    
+        
+    #print(query_vec_ind)
+    #    
+    #for each_query in query_entries: 
+    #    print("Shopping Cart: ", *each_query)
+    #    
+    #    for individual_item in each_query:
+    #        
+    #        item_angle = []
+    #        
+    #        print("Item: ", individual_item)
+    #    
+    #        print(query_vec_ind[(int(individual_item) - 1)])
+    #    
+    #        for all_history_vec in history_vec:
+    #                
+    #            if np.any(np.not_equal((query_vec_ind[(int(individual_item) - 1)]), all_history_vec)):
+    #                
+    #                item_angle.append(calculate_angle((query_vec_ind[(int(individual_item) - 1)]), all_history_vec))
+    #
+    #                print(min(item_angle))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
                 
-                
-    
     print("Positive Entries: ", positive_entries)
     print("Average Angle: ", avg_angle)
+    
+    query_no = 0
+    
+    for each_query in query_entries: 
+        print("Shopping Cart: ", *each_query)
+        
+        for individual_item in each_query:
+            no_in_list = (int(individual_item) - 1)
+            
+            print("Item: ", individual_item, "Angle: ", min(all_angles[no_in_list]), 
+                  "Match: ", np.argmin(all_angles[no_in_list]))
+            
+            
+            
+            #print("Angle: ", str(round(angle, 2)))
+            
+            
+        query_no = query_no + 1
+        
+    
+    
+    
+    
 		
 def history_vectors():
     
@@ -94,8 +168,9 @@ def calculate_angle(x, y):
     
     cos_theta = np.dot(x, y) / (norm_x * norm_y)
     theta = math.degrees(math.acos(cos_theta))
+    rounded_theta = round(theta, 2)
 
-    return theta       
+    return rounded_theta       
 
 def convert_history_to_np_dtype():
     
@@ -108,6 +183,14 @@ def convert_history_to_np_dtype():
 def convert_queries_to_np_dtype():
     
     all_vectors = query_vectors()
+    
+    all_arrays = np.array(list(all_vectors.values()))
+    
+    return all_arrays
+
+def convert_queries_to_np_dtype_individual():
+    
+    all_vectors = query_vectors_individual()
     
     all_arrays = np.array(list(all_vectors.values()))
     
@@ -126,6 +209,36 @@ def average_angle():
                 all_angles.append(calculate_angle(x_vector, y_vector))
                 
     return (np.average(all_angles))
+
+
+def query_vectors_individual():
+    
+    history_data = open_history()
+    header_row = history_data[0]
+    total_items = header_row[1]
+    #print(total_items)
+    
+    query_number = 0
+    result_dictionary = {}  
+    
+    for queries in range(int(total_items)):
+        array_of_items = []
+        
+        for items in range(int(total_items)):
+            array_of_items.append(0)
+            
+        result_dictionary.update({query_number : array_of_items})
+        query_number = query_number + 1  
+    
+    for key, value in result_dictionary.items():
+        
+        result_dictionary[key][key] = 1
+                          
+    return result_dictionary
+    
+    
+    
+
             
 def query_vectors():
     
